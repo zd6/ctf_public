57c7e82ab09f76e8266aa4c0216760ce9e5ef3cf
Seung Hyun Kim <skim0119@gmail.com>
2020-02-11 19:07:29 -0600

    add external package data

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name="gym_cap",
       url="https://https://github.com/raide-project/ctf_public",
       packages=["gym_cap", "gym_cap.envs"],
       zip_safe=False,
+      include_package_data=True,
       install_requires = [
           "gym>=0.16.0",
           "pygame>=1.3.2",
