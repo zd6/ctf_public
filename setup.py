89d30b2c13fd4d021b9927c8e8951ea26e5663d0
Seung Hyun Kim <skim0119@gmail.com>
2020-02-11 19:05:34 -0600

    udpate setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 setup(name="gym_cap",
-      version="1.0",
-      author="DOs",
+      version="1.0.0",
+      author="DOs, skim449",
       license="MIT",
+      url="https://https://github.com/raide-project/ctf_public",
       packages=["gym_cap", "gym_cap.envs"],
       zip_safe=False,
-      install_requires = ["gym", "pygame", "numpy"]
+      install_requires = [
+          "gym>=0.16.0",
+          "pygame>=1.3.2",
+          "numpy>=1.18.1"
+          ]
 )
