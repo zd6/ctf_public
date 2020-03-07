02e43f61cfa324bc0983d9e37c757ca0e400d8ee
Seung Hyun Kim <skim0119@gmail.com>
2020-03-07 15:13:15 -0600

    fix url of setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.2",
     license="MIT",
     author="DOs, skim449",
-    url="https://https://github.com/raide-project/ctf_public",
+    url="https://github.com/raide-project/ctf_public",
     packages=["gym_cap", "gym_cap.envs", "gym_cap.heuristic"],
     zip_safe=False,
     include_package_data=True,
